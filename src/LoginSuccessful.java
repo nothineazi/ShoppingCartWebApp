6d068fe93a2884754f2d4ee3af87935131830236
nothineazi <73596248+nothineazi@users.noreply.github.com>
2022-01-15 00:53:37 +0600

    Update LoginSuccessful.java

--- src/LoginSuccessful.java
+++ src/LoginSuccessful.java
@@ -5,9 +5,24 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class LoginSuccessful extends HttpServlet {
+    
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    Cookie[] cookies = request.getCookies();
+    String user = null;
+    if(cookies)==null{
+        response.sendRedirect("Login.html");
     }
+    else{
+        for(Coolie cookie:cookies){
+            if(cookie.getname().equals("currentUser")){
+                user=cookie.getValue();
+            }
+        }
+    }
+        PrintWriter out=response.getWriter();
+        out.println("<h1>Welcome, " +user+ "</h1>");
+        out.println(<form method=\"post\"action=\"LogoutServlet\"> <input type=\"submit\"value=\"Log Out\"> </form>);
+           
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
